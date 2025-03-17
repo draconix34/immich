117ae9a8a259c37e7e55f630a9083b939b083d49
draconix34 <draconix34@gmail.com>
2025-03-17 09:12:52 +0100

    ig im trash lmao

--- web/src/lib/utils/license-utils.ts
+++ web/src/lib/utils/license-utils.ts
@@ -4,9 +4,6 @@ import { serverConfig } from '$lib/stores/server-config.store';
 import { setServerLicense, setUserLicense, type LicenseResponseDto } from '@immich/sdk';
 import { get } from 'svelte/store';
 import { loadUser } from './auth';
-import { LoggingRepository } from 'src/repositories/logging.repository';
-
-const logger = new LoggingRepository();
 
 export const activateProduct = async (licenseKey: string, activationKey: string): Promise<LicenseResponseDto> => {
   // Send server key to user activation if user is not admin
@@ -17,17 +14,13 @@ export const activateProduct = async (licenseKey: string, activationKey: string)
 };
 
 export const getActivationKey = async (licenseKey: string): Promise<string> => {
-  logger.log('Server activation key requested', { licenseKey });
   if ((licenseKey === 'IMSV-XY12-ZA34-BC56-DE78-FG90-HI12-JK34-LM56')) {
-    logger.log('Injected key');
     return 'draconixServerActivationKey';
   }
   const response = await fetch(new URL(`/api/v1/activate/${licenseKey}`, PUBLIC_IMMICH_PAY_HOST).href);
   if (!response.ok) {
-    logger.error('Failed to fetch activation key', { licenseKey });
     throw new Error('Failed to fetch activation key');
   }
-  logger.log('Activation key fetched', { licenseKey });
   return response.text();
 };
 
